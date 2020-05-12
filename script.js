db9a08567156f63e4e54e2b1a08713a91cff7cc3
arozbiz <arozbiz@gmail.com>
2020-05-11 19:26:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const tags = ['c', 'qt', 'obj']; // these are my tags: comment, blockquote, and 
 function applyAndHideTag(tags) {
     tags.forEach((tag) => {
         document.querySelectorAll('span[data-tag=' + tag + ']').forEach((node) => {
-            if (node.closest('div').classList.contains(tag + '_tag') == false) { // this check is so that this function only runs when necessary and doesn't cause the mutationObserver to go into an infinite loop
-                node.closest('div').classList.add(tag + '_tag');
+            if (node.closest('div.roam-block-container').classList.contains(tag + '_tag') == false) { // this check is so that this function only runs when necessary and doesn't cause the mutationObserver to go into an infinite loop
+                node.closest('div.roam-block-container').classList.add(tag + '_tag');
                 node.parentNode.innerHTML = node.parentNode.innerHTML.replace('</span> ', '</span>'); //since the custom CSS will hide the tag, this gets ready of the resulting leading whitespace
             }
         });
